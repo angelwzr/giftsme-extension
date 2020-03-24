99caa8ddd5445af476f548adb0f2cf4957b89bc4
angelwzr <angel@windowz.ru>
2020-03-24 22:20:37 +0300

    New tab fix

--- js/background.js
+++ js/background.js
@@ -3,6 +3,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         var currentUrl = tabs[0].url;
         var newUrl =  "http://gogifts.me/?url=" + currentUrl
-        chrome.tabs.update({url:newUrl});
+        chrome.tabs.create({url:newUrl});
     });
 });
