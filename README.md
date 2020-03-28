f78aa0b72fbd5a7e9891db0872e8d4a1ee389a55
angelwzr <angel@windowz.ru>
2020-03-28 16:36:38 +0300

    1.0.3
    Keyboard shortcuts, code refinements, README.md update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # giftsme-extension
-gogifts.me browser extension
+### gogifts.me browser extension
+A simple browser extension to add current tab page to the GIFTS.ME wishlist.
+
+### Browser support
+|Browser|Download|Note|
+|--|--|--|
+|Google Chrome|n/a|Fully supported|
+|Microsoft Edge|n/a|Fully supported|
+|Opera|n/a|Fully supported|
+|Mozilla Firefox|n/a|Fully supported|
--- js/background.js
+++ js/background.js
@@ -1,8 +1,18 @@
 
-chrome.browserAction.onClicked.addListener(function(tab) { 
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+function onClicked() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var currentUrl = tabs[0].url;
-        var newUrl =  "http://gogifts.me/?url=" + currentUrl
-        chrome.tabs.create({url:newUrl});
+        var newUrl = "http://gogifts.me/?url=" + currentUrl
+        //uncomment for debug purposes
+        //alert(newUrl)
+        chrome.tabs.create({ url: newUrl });
     });
+}
+
+chrome.browserAction.onClicked.addListener(onClicked);
+
+chrome.commands.onCommand.addListener(function (command) {
+    if (command == "keyblock") {
+        onClicked();
+    }
 });
